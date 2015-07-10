f56a9c7655ce270ea0e52805c9bd05cad66a64f1
demolish <xdemolish@hotmail.com>
2015-07-10 15:37:46 +0100

    CORE: added debug prints in SendToZone (which really should never print unless someone gets zoned while shutting down)

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -4696,10 +4696,15 @@ namespace charutils
 
     void SendToZone(CCharEntity* PChar, uint8 type, uint64 ipp)
     {
+        if (PChar->status == STATUS_SHUTDOWN || PChar->status == STATUS_DISAPPEAR)
+        {
+            ShowDebug("Player <%s> is already zoning. Unable to %s.\n", PChar->GetName(), PChar->status == STATUS_SHUTDOWN ? "shutdown" : "zone");
+            return;
+        }
         if (type == 2)
         {
             Sql_Query(SqlHandle, "UPDATE accounts_sessions SET server_addr = %u, server_port = %u WHERE charid = %u;",
-                      (uint32)ipp, (uint32)(ipp >> 32), PChar->id);
+                (uint32)ipp, (uint32)(ipp >> 32), PChar->id);
 
             const int8* Query =
                 "UPDATE chars "
@@ -4714,22 +4719,21 @@ namespace charutils
                 "WHERE charid = %u;";
 
             Sql_Query(SqlHandle, Query,
-                      PChar->loc.destination,
-                      PChar->m_moghouseID ? 0 : PChar->getZone(),
-                      PChar->loc.p.rotation,
-                      PChar->loc.p.x,
-                      PChar->loc.p.y,
-                      PChar->loc.p.z,
-                      PChar->loc.boundary,
-                      PChar->id);
+                PChar->loc.destination,
+                PChar->m_moghouseID ? 0 : PChar->getZone(),
+                PChar->loc.p.rotation,
+                PChar->loc.p.x,
+                PChar->loc.p.y,
+                PChar->loc.p.z,
+                PChar->loc.boundary,
+                PChar->id);
         }
         else
         {
             SaveCharPosition(PChar);
         }
 
-        if (PChar->status != STATUS_SHUTDOWN && PChar->status != STATUS_DISAPPEAR)
-            PChar->pushPacket(new CServerIPPacket(PChar, type, ipp));
+        PChar->pushPacket(new CServerIPPacket(PChar, type, ipp));
     }
 
     int32 GetVar(CCharEntity* PChar, const char* var)
